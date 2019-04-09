6065edaf44e6b9dbe512bfb3ca56f306b2621b38
Daniel <danielc1015@gmail.com>
2019-04-09 17:26:54 -0400

    Methods for get user image and get user details created. Routes created too

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use App\User;
 
 class UserController extends Controller
@@ -205,6 +206,43 @@ class UserController extends Controller
                 'message' => 'Imagen subida correctamente'
             );
    
+        }
+
+        return response()->json($data, $data['code']);
+    }
+
+
+    public function getImage($filename){
+        $isset = \Storage::disk('users')->exists($filename);
+        if ($isset) {
+            $file = \Storage::disk('users')->get($filename);
+            return new Response($file, 200);
+        }else{
+            $data = array(
+                'code' => 404,
+                'status' => 'error',
+                'message' => 'La imagen no existe'
+            );
+            return response()->json($data, $data['code']);
+        }
+        
+    }
+
+    public function detail($id){
+        $user = User::find($id);
+
+        if (is_object($user)) {
+            $data = array(
+                'code' => 200,
+                'status' => 'success',
+                'user' => $user
+            );
+        } else {
+            $data = array(
+                'code' => 404,
+                'status' => 'error',
+                'message' => 'El usuario no existe'
+            );
         }
 
         return response()->json($data, $data['code']);
--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,5 @@ Route::post('api/register', 'UserController@register');
 Route::post('/api/login', 'UserController@login');
 Route::put('/api/user/update', 'UserController@update');
 Route::post('/api/user/upload', 'UserController@upload')->middleware(ApiAuthMiddleware::class);
+Route::get('/api/user/avatar/{filename}', 'UserController@getImage');
+Route::get('/api/user/detail/{id}', 'UserController@detail');
